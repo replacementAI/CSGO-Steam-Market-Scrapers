cbc4a725ba1b53c3166dc7e4b5a7a65c239144e7
replacementAI <55959390+replacementAI@users.noreply.github.com>
2023-04-14 14:21:25 -0500

    Create LOB Scraper.py

---
+++ lambda/LOB Scraper.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import json
+import urllib.parse
+import time
+import boto3
+import os
+
+s3 = boto3.client('s3')
+os.chdir('/tmp')
+
+def lambda_handler(event, context):
+    URL = 'https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid=176358765&two_factor=0'
+    # replace item_nameid
+    
+    filename = f'{time.strftime("%Y-%m-%d %H:%M", time.localtime())} revolution case.csv'
+    
+    dataframe = pd.DataFrame(columns=['date', 'buy_price', 'buy_volume', 'sell_price', 'sell_volume'])
+
+    soup = BeautifulSoup(requests.get(URL).text, 'html.parser').text
+
+    json_data = json.loads(soup)
+
+    try:
+        buy_order = json_data['buy_order_graph'][0]
+        sell_order = json_data['sell_order_graph'][0]
+    except TypeError:
+
+        order_dict = {'date': time.strftime('%Y-%m-%d %H:%M', time.localtime()),
+                        'buy_price': 'NaN',
+                        'buy_volume': 'NaN',
+                        'sell_price': 'NaN',
+                        'sell_volume': 'NaN',
+                        }
+        dataframe = dataframe.append(order_dict, ignore_index=True)
+
+        return dataframe.to_csv(filename, index=False, header=True, mode='w')
+
+    highest_buy_order = buy_order[:-1]
+    lowest_sell_order = sell_order[:-1]
+
+    order_dict = {'date': time.strftime('%Y-%m-%d %H:%M', time.localtime()),
+                  'buy_price': highest_buy_order[0],
+                  'buy_volume': highest_buy_order[1],
+                  'sell_price': lowest_sell_order[0],
+                  'sell_volume': lowest_sell_order[1],
+                  }
+    dataframe = dataframe.append(order_dict, ignore_index=True)
+    
+    dataframe.to_csv(filename, index=False, header=True, mode='w')
+    
+    s3.upload_file(filename, os.environ['BUCKET_NAME'], filename)
